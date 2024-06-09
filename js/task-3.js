b58bdf9d0cc9b1610991de934dabfe75de86781d
Andrii Sheremeta <162326622+m9ta92@users.noreply.github.com>
2024-06-09 13:08:41 +0200

    Add files via upload

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+function getElementWidth(content, padding, border) {
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding);
+  const borderWidth = parseFloat(border);
+  const totalWidth = parseFloat(
+    contentWidth + 2 * paddingWidth + 2 * borderWidth
+  );
+  console.log(totalWidth);
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
